7bee96ddfcf8419a55f33af69861c477e4950726
Aytekin Yaliz <aytekinyaliz@gmail.com>
2020-04-12 02:28:17 +0100

    update

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -6,3 +6,4 @@ export { BaseSchema } from "./src/repositories/base/BaseSchema";
 export { IVersionableDocument } from "./src/repositories/versionable/IVersionableDocument";
 export { VersionableRepository } from "./src/repositories/versionable/VersionableRepository";
 export { VersionableSchema } from "./src/repositories/versionable/VersionableSchema";
+export * from "./src/libs/utilities";
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,7 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
 var auditSchema_1 = require("./src/repositories/audit/auditSchema");
 exports.auditSchema = auditSchema_1.auditSchema;
@@ -10,4 +13,5 @@ var VersionableRepository_1 = require("./src/repositories/versionable/Versionabl
 exports.VersionableRepository = VersionableRepository_1.VersionableRepository;
 var VersionableSchema_1 = require("./src/repositories/versionable/VersionableSchema");
 exports.VersionableSchema = VersionableSchema_1.VersionableSchema;
+__export(require("./src/libs/utilities"));
 //# sourceMappingURL=index.js.map
